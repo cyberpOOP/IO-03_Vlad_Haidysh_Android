2c4dcc3b90bdf9f4223883d6d6269053f147c243
CyberpOOP <vlad0021135@gmail.com>
2022-10-14 14:31:16 +0300

    Lab2 added

---
+++ Lab2/app/src/main/java/com/example/lab2/MainFragment.java
@@ -0,0 +1,26 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void UpdateText(String text){
+        TextView TextRes = (TextView) getView().findViewById(R.id.textView);
+        TextRes.setText(text);
+    }
+
+}
