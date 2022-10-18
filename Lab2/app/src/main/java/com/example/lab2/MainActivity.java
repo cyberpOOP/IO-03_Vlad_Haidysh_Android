dd399209bb1a2e84f83b03af5792b22c27ed976c
CyberpOOP <vlad0021135@gmail.com>
2022-10-18 11:33:05 +0300

    interface working

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -12,13 +12,26 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    InputFragment fragmentInput = new InputFragment();
+    ResultFragment fragmentResult = new ResultFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SetFragments();
+    }
+
+    private void SetFragments() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_input, fragmentInput);
+        transaction.replace(R.id.frame_result, fragmentResult);
+        transaction.commit();
     }
 }
