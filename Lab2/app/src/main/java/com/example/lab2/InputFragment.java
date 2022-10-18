747e3b10ea3a0a368946ee99a1f391e19d263c9a
CyberpOOP <vlad0021135@gmail.com>
2022-10-18 11:02:11 +0300

    cleaned code

--- Lab2/app/src/main/java/com/example/lab2/InputFragment.java
+++ Lab2/app/src/main/java/com/example/lab2/InputFragment.java
@@ -7,20 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class MainFragment extends Fragment {
-
+public class InputFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_input, container, false);
     }
-
-    public void UpdateText(String text){
-        TextView TextRes = (TextView) getView().findViewById(R.id.textView);
-        TextRes.setText(text);
-    }
-
 }
--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,113 +1,24 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Set;
-
 
 public class MainActivity extends AppCompatActivity {
-
-    private RadioGroup ColorGroup;
-    private RadioGroup BunchGroup;
-    private RadioButton ColorButton;
-    private RadioButton BunchButton;
-    private TextView TextRes;
-    private EditText Text;
-    MainFragment Fragment = new MainFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ColorGroup = findViewById(R.id.RadioColor);
-        BunchGroup = findViewById(R.id.RadioSize);
-
-        Button ButtonOK = findViewById(R.id.button);
-        Text = findViewById(R.id.TextField);
-
-        SetFragment();
-
-
-        ButtonOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int[] mass = new int[3];
-                boolean let = true;
-                mass[0] = Text.getText().length();
-                mass[1] = ColorGroup.getCheckedRadioButtonId();
-                mass[2] = (int)BunchGroup.getCheckedRadioButtonId()-1;
-
-                for(int i : mass)
-                    switch (i){
-                        case 0:
-                            Toast.makeText(getApplicationContext(), "Comment is empty",
-                                    Toast.LENGTH_SHORT).show();
-                            TextRes.setText(R.string.TextView);
-                            let = false;
-                            break;
-                        case -1:
-                            Toast.makeText(getApplicationContext(), "Choose flowers color",
-                                    Toast.LENGTH_SHORT).show();
-                            let = false;
-                            break;
-                        case -2:
-                            Toast.makeText(getApplicationContext(), "Choose bunch size",
-                                    Toast.LENGTH_SHORT).show();
-                            let = false;
-                            break;
-                    }
-
-                if(let){
-                    ColorButton = findViewById(ColorGroup.getCheckedRadioButtonId());
-                    BunchButton = findViewById(BunchGroup.getCheckedRadioButtonId());
-                    int PriceId = BunchGroup.getCheckedRadioButtonId();
-                    String price ="";
-                    String order;
-                    switch(PriceId){
-                        case 2131230733:
-                            price = "20$";
-                            break;
-                        case 2131230730:
-                            price = "30$";
-                            break;
-                        case 2131230727:
-                            price = "45$";
-                            break;
-                        case -1:
-                            price ="10$";
-                            break;
-                    }
-                    order = BunchButton.getText().toString()+" of " +
-                            ColorButton.getText().toString()+" roses.";
-
-                    /*TextRes.setText("Your Order is:\n"+order+"\n"+
-                            "Your Comment:\n"+Text.getText()+"\nPrice: "+price);*/
-                    Fragment.UpdateText("Your Order is:\n"+order+"\n"+
-                            "Your Comment:\n"+Text.getText()+"\nPrice: "+price);
-                    SetFragment();
-
-                }
-
-            }
-        });
-    }
-
-    private void SetFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, Fragment);
-        transaction.commit();
     }
 }
