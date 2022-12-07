dbe5bc5595cdc8783b79b7bb0a87cf2454accdfc
CyberpOOP <vlad0021135@gmail.com>
2022-12-07 13:44:17 +0200

    working fragments

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.java
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button video = findViewById(R.id.video);
+        Button audio = findViewById(R.id.audio);
+
+        Video videoFragment = new Video();
+        Audio audioFragment = new Audio();
+
+        video.setOnClickListener(view -> setFragment(videoFragment));
+        audio.setOnClickListener(view -> setFragment(audioFragment));
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, fragment);
+        transaction.commit();
     }
 }
